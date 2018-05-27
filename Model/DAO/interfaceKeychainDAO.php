73f31c099ecf449d553ffa6095f9e86ce00589f6
GuillaumeRL <guillaumeerhel@gmail.com>
2018-05-27 14:11:31 +0200

    Ajout des fichiers de base

---
+++ Model/DAO/interfaceKeychainDAO.php
@@ -0,0 +1,13 @@
+<?php
+
+interface interfaceKeyChainDAO
+{
+
+    // Singleton
+    public static function getInstance();
+
+    public function getKeychains();
+
+    public function getRandomKeychain();
+
+}
