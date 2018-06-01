dcb2f79d80feb50bc9a9df53c48400b91d35da83
Jeffrey Bataille <jbataill@enssat.fr>
2018-06-01 17:45:04 +0200

    coucou

---
+++ Inventory.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 01/06/18
+ * Time: 17:33
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Inventory</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+</head>
+<body>
+<header><h1>Inventaire</h1></header>
+<nav class="panel panel-default">
+    <div class="panel-header"><h4>Menu</h4></div>
+    <div class="panel-body"><a href="#">Accueil</a></div>
+    <div class="panel-body"><a href="#">Inventaire</a></div>
+    <div class="panel-body"><a href="#">Ressource</a></div>
+    <div class="panel-body"><a href="#">Gestion</a></div>
+</nav>
+</body>
+</html>
