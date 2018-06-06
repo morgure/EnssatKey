210c7ee7b8d59df74757052c830196043c75f68d
Jeffrey Bataille <jbataill@enssat.fr>
2018-06-06 11:30:49 +0200

    Add attribut idRoom & idLock

--- Model/VO/DoorVO.php
+++ Model/VO/DoorVO.php
@@ -2,13 +2,28 @@
 class DoorVO
 {
     protected $id;
+    protected $lock;
+    protected $room;
 
     public function setId($id) {
         $this->id = $id;
+    }
+    public function setLock($id) {
+        $this->lock=$id;
+    }
+    public function setRoom($id) {
+        $this->room=$id;
     }
 
     public function getId() {
         return $this->id;
     }
+    public function getLock() {
+        return $this->lock;
+    }
+    public function getRoom() {
+        return $this->room;
+    }
+
 
 }
