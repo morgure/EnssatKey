f91534bbd9da182a6a937825cd7151f15eb4f007
Aurelien <Aurelien@10.41.61.173>
2018-06-12 16:27:08 +0200

    xml all update

--- Model/VO/KeyVO.php
+++ Model/VO/KeyVO.php
@@ -1,25 +1,20 @@
 <?php
 class KeyVO
 {
-
     public static $keyType = array("Simple"=>"Clé","Partiel"=>"Passe Partiel","Total"=>"PasseTotal");
-
     protected $id;
     protected $type; //Clef ou Passe Partiel ou Passe Total
-
+    protected $keychain;
     public function __construct($type)
     {
       $this->type=$type;
     }
-
     public function setId($id) {
         $this->id = $id;
     }
-
     public function getId() {
         return $this->id;
     }
-
     public function setType($type) {
       if(array_key_exists($type,$this->keyType)){
         $this->type = $type;
@@ -29,9 +24,22 @@ class KeyVO
         throw new RuntimeException('Le type de clef <strong>' . $type . '</strong> n\'existe pas !');
       }
     }
-
     public function getType() {
         return $this->type;
     }
-
+    /**
+     * @param mixed $keychain
+     */
+    public function setKeychain($keychain)
+    {
+        $this->keychain = $keychain;
+    }
+    //Jeffrey Bataille
+    /**
+     * @return mixed
+     */
+    public function getKeychain()
+    {
+        return $this->keychain;
+    }
 }
