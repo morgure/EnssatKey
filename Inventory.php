7ceaf5b737903fb448d70aaaf360c5d93b330f45
Mathieu Crusson <mcrusson@enssat.fr>
2018-06-13 11:53:14 +0200

    Merge branch 'master' of https://github.com/morgure/EnssatKey
    # Conflicts:
    #	Model/DAO/implementationLockDAO_Dummy.php

--- Inventory.php
+++ Inventory.php
@@ -22,12 +22,83 @@
             <a class="navbar-brand" href="#">EnssatKey</a>
         </div>
         <ul class="nav navbar-nav">
-            <li><a href="#">Accueil</a></li>
-            <li class="active"><a href="#">Inventaire</a></li>
-            <li><a href="#">Page 2</a></li>
+            <li><a href="index.php">Accueil</a></li>
+            <li class="active"><a href="Inventory.php">Inventaire</a></li>
+            <li><a href="Ressources.php">Ressources</a></li>
             <li><a href="#">Gestion</a></li>
         </ul>
     </div>
 </nav>
+
+<div class="container">
+        <h2>En circulation</h2>
+        <p>Cles en circulation</p>
+        <table class="table table-hover">
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>date</th>
+                <th>proprietaire</th>
+                <th>type d'acces</th>
+                <th>Type de clé </th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>45684</td>
+                <td>12/02/2002</td>
+                <td>john</td>
+                <td>acces enseignant</td>
+                <td>passe presque partout</td>
+            </tr>
+            <tr>
+                <td>12548</td>
+                <td>25/12/2015</td>
+                <td>David</td>
+                <td>acces etudiant</td>
+                <td>clef unique</td>
+            </tr>
+            <tr>
+                <td>98547</td>
+                <td>13/01/1985</td>
+                <td>Christophe</td>
+                <td>acces illimté</td>
+                <td>passe partout</td>
+            </tr>
+            </tbody>
+        </table>
+
+    <div class="MenuInventaire">
+        <label for="date">Date prolongement</label>
+        <input type="date" class="form-control" id="usr">
+        <button type="button" class="btn btn-default">prolonger</button>
+
+        <button type="button" class="btn btn-default">Dupliquer trousseau</button>
+    </div>
+
+<h2>Trousseau</h2>
+
+    <table class="table table-hover">
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>Nb clefs</th>
+            <th>IDclefs</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>T1</td>
+            <td>7</td>
+            <td>1/2/3/4/5/6/7</td>
+        </tr>
+        <tr>
+            <td>T2</td>
+            <td>4</td>
+            <td>8/9/10</td>
+        </tr>
+        </tbody>
+    </table>
+    </div>
 </body>
 </html>
